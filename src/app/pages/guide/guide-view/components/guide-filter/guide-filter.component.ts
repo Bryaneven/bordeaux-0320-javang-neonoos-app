591a517685e11ba88cc55ada2dcec132bd09a235
Deligny Benoit <delignyb.pro@gmail.com>
2020-06-01 16:26:48 +0200

    feat: add guide filter component

---
+++ src/app/pages/guide/guide-view/components/guide-filter/guide-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'neo-guide-filter',
+  templateUrl: './guide-filter.component.html',
+  styleUrls: ['./guide-filter.component.scss']
+})
+export class GuideFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
