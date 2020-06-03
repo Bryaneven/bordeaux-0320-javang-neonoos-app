b180d2a072247a1bc14abc79c9fa6f6b3f2627ad
Xavier <x.crochet@gmail.com>
2020-06-04 00:24:21 +0200

    feat: guide-filter: finished input search + button new section

--- src/app/pages/guide/guide.module.ts
+++ src/app/pages/guide/guide.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Material
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
 
 import { GuideRoutingModule } from './guide-routing.module';
 import { GuideFilterComponent } from './guide-view/components/guide-filter/guide-filter.component';
 import { GuideResultsComponent } from './guide-view/components/guide-results/guide-results.component';
 import { GuideViewComponent } from './guide-view/guide-view.component';
+
 
 
 @NgModule({
@@ -15,7 +21,9 @@ import { GuideViewComponent } from './guide-view/guide-view.component';
     CommonModule,
     GuideRoutingModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ]
 })
 export class GuideModule { }
