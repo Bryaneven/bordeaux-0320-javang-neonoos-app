f8a63cdab80dc6405567a18040fff45cc8f6ef22
Gabriel Touron <gtcode@pm.me>
2020-06-08 16:13:37 +0200

    feat(guide-edit): add git-edit child components

---
+++ src/app/pages/guide/guide-edit-view/components/guide-travel/guide-travel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'neo-guide-travel',
+  templateUrl: './guide-travel.component.html',
+  styleUrls: ['./guide-travel.component.scss']
+})
+export class GuideTravelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
