f8e1266b40c67e78fee62442d74fef1eb513dd6a
Gabriel Touron <gtcode@pm.me>
2020-06-11 09:50:13 +0200

    feat(hashtag): add matchips for hashtag

--- src/app/pages/guide/guide.module.ts
+++ src/app/pages/guide/guide.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GoogleMapsModule } from '@angular/google-maps'
+import { GoogleMapsModule } from '@angular/google-maps';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Material
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
+import {MatIconModule} from '@angular/material/icon';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDividerModule} from '@angular/material/divider';
-
+import {FormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { GuideRoutingModule } from './guide-routing.module';
 import { GuideFilterComponent } from './guide-view/components/guide-filter/guide-filter.component';
@@ -55,6 +57,9 @@ import { GuidePoiSearchSectionComponent } from './guide-edit-view/components/gui
     MatExpansionModule,
     MatSelectModule,
     MatDividerModule,
+    FormsModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
     GoogleMapsModule
   ]
 })
