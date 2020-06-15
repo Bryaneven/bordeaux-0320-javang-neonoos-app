30c502cc62f2305aec238f2e5c9bb139db39bbe1
Xavier <x.crochet@gmail.com>
2020-06-15 16:15:44 +0200

    feat: (place) Add place-filter, place-results in components

---
+++ src/app/pages/place/place-view/components/place-results/place-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'neo-place-results',
+  templateUrl: './place-results.component.html',
+  styleUrls: ['./place-results.component.scss']
+})
+export class PlaceResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
