bc3e58fc11db776fbe2e88838a842dc494ce531d
Xavier <x.crochet@gmail.com>
2020-06-15 14:52:29 +0200

    feat: (place) Add component place/place-view and place-edit

--- src/app/pages/place/place.module.ts
+++ src/app/pages/place/place.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlaceRoutingModule } from './place-routing.module';
+import { PlaceViewComponent } from './place-view/place-view.component';
+import { PlaceEditComponent } from './place-edit/place-edit.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PlaceViewComponent, PlaceEditComponent],
   imports: [
     CommonModule,
     PlaceRoutingModule
