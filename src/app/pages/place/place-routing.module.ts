6d94bdf138f1e548d20b671aba5dfd23a5305db8
Xavier <x.crochet@gmail.com>
2020-06-15 15:04:12 +0200

    feat: (place) routing place - place/new - place-edit

--- src/app/pages/place/place-routing.module.ts
+++ src/app/pages/place/place-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlaceViewComponent } from './place-view/place-view.component';
+import { PlaceEditComponent } from './place-edit/place-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'view'
+  },
+  {
+    path: 'view',
+    component: PlaceViewComponent
+  },
+  {
+    path: 'new',
+    component: PlaceEditComponent
+  },
+  {
+    path: 'edit/:id',
+    component: PlaceEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
