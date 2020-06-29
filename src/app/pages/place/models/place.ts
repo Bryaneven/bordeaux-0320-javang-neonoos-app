701b89392afeb604a74c59c28ab9e758a7c1f494
couettou <delignyb.pro@gmail.com>
2020-06-29 14:56:42 +0200

    feat: fill place class

--- src/app/pages/place/models/place.ts
+++ src/app/pages/place/models/place.ts
@@ -1,2 +1,43 @@
+import { CreateTracingOptions } from 'trace_events';
+import { Hashtag } from 'src/app/shared/models/hashtag';
+import { Guide } from '../../guide/models/guide';
+
 export class Place {
+  id: number;
+  name: string;
+  description?: string;
+  type: string;
+  osm?: number;
+  lat: number;
+  'lat_min'?: number;
+  'lat_max'?: number;
+  lng: number;
+  'lng_min'?: number;
+  'lng_max'?: number;
+  plc: boolean;
+  draft: boolean;
+  'price_range'?: number;
+  rating: number;
+  'rating_count': number;
+  genius: boolean;
+  'genius_stars'?: number;
+  // categories: Category[];
+  adress?: string;
+  city?: string;
+  // countries: Country[];
+  // creator: Creator;
+  created?: Date;
+  updated?: Date;
+  searchables?: string;
+  barometer?: number;
+  hashtags: Hashtag[];
+  'area_level'?: number;
+  'base_rating'?: string;
+  weight: number;
+  guides: Guide[];
+  'total_stars': number;
+
+  constructor(place: Place) {
+    Object.assign(this, Place);
+  }
 }
