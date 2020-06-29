0587af763b738173bf3900dc99e71bec3f81f7db
Gabriel Touron <56262724+GabrielTouron@users.noreply.github.com>
2020-06-29 16:12:43 +0200

    Feat/abstract elide class (#23)
    * feat(models): add abstract classes
    
    * feat(guide): fix guide
    
    
    Co-authored-by: Gabriel Touron <gtcode@pm.me>

--- src/app/pages/guide/services/guide/guide.service.ts
+++ src/app/pages/guide/services/guide/guide.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Guide } from '../../models/guide';
+import { RootObject } from 'src/app/shared/models/root-object.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,27 +13,28 @@ export class GuideService {
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<Guide[]> {
-    return this.http.get<Guide[]>(`${environment.APIURI}guides`).pipe(map((guides) => guides.map((guide) => this.createInstance(guide))));
+  getAll(): Observable<RootObject<Guide>[]> {
+    return this.http.get<RootObject<Guide>[]>(`${environment.APIURI}guides`).pipe(map((guides) => guides
+    .map((guide) => this.createInstance(guide))));
   }
 
-  getById(id: number): Observable<Guide>{
-    return this.http.get<Guide>(`${environment.APIURI}guides/` + id).pipe(map((guide) => this.createInstance(guide)));
+  getById(id: number): Observable<RootObject<Guide>>{
+    return this.http.get<RootObject<Guide>>(`${environment.APIURI}guides/` + id).pipe(map((guide) => this.createInstance(guide)));
   }
 
-  post(guide: Guide): Observable<Guide>{
-    return this.http.post<Guide>(`${environment.APIURI}guides`, guide).pipe(map((guidefrmsrv) => this.createInstance(guidefrmsrv)));
+  post(guide: RootObject<Guide>): Observable<RootObject<Guide>>{
+    return this.http.post<RootObject<Guide>>(`${environment.APIURI}guides`, guide).pipe(map((guidefrmsrv) => this.createInstance(guidefrmsrv)));
   }
 
-  patch(guide: Guide, id: number): Observable<Guide>{
-    return this.http.patch<Guide>(`${environment.APIURI}guides/` + id, guide).pipe(map((guidefrmsrv) => this.createInstance(guidefrmsrv)));
+  patch(guide: RootObject<Guide>, id: number): Observable<RootObject<Guide>>{
+    return this.http.patch<RootObject<Guide>>(`${environment.APIURI}guides/` + id, guide).pipe(map((guidefrmsrv) => this.createInstance(guidefrmsrv)));
   }
 
-  delete(id: number): Observable<Guide>{
-    return this.http.delete<Guide>(`${environment.APIURI}guides/` + id);
+  delete(id: number): Observable<void>{
+    return this.http.delete<void>(`${environment.APIURI}guides/` + id);
   }
 
-  createInstance(guide: Guide){
-    return new Guide(guide);
+  createInstance(guide: RootObject<Guide>){
+    return new RootObject<Guide>(Guide, guide);
   }
 }
