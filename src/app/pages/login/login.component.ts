4c27e9e65e503ff54045a8d6c1af1140c53a5e8b
couettou <delignyb.pro@gmail.com>
2020-07-04 16:59:10 +0200

    feat: add service and submit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { AuthService } from './services/auth.service';
+import { User } from './models/user';
+import { RootObjectList } from 'src/app/shared/models/root-object-list.model';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -16,7 +19,10 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class LoginComponent implements OnInit {
 
+  users: RootObjectList<User>;
 
+
+  /* Formulaire */
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
@@ -27,13 +33,15 @@ export class LoginComponent implements OnInit {
 
   matcher = new MyErrorStateMatcher();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  /* submit du formulaire */
   OnSubmit(email: string, password: string) {
-    console.log(email, password);
+    this.authService.loginUser(email, password).subscribe();
+    // store dans le local storage le token si il y est
   }
 
 }
