953cd8cd8d1867a7a368f6a402a66c9b8d88b385
couettou <delignyb.pro@gmail.com>
2020-07-04 16:58:08 +0200

    feat: add login method

--- src/app/pages/login/services/auth.service.ts
+++ src/app/pages/login/services/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { RootObject } from 'src/app/shared/models/root-object.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  loginUser(email: string, password: string): Observable<RootObject<User>> {
+    return this.httpClient.post<RootObject<User>>('http://localhost:8080/login', {email, password});
+  }
 }
