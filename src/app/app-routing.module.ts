cfcd2a827467eab2ebde0544a90214146109fde2
couettou <delignyb.pro@gmail.com>
2020-07-06 11:39:08 +0200

    feat: add guard to routes (but guard not OK)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/guard/auth.guard';
 
 
 const routes: Routes = [
@@ -10,12 +11,14 @@ const routes: Routes = [
   },
   {
     path: 'guide',
+    canActivate: [AuthGuard],
     loadChildren: () => import('../app/pages/guide/guide.module').then(
       m => m.GuideModule
     )
   },
   {
     path: 'place',
+    canActivate: [AuthGuard],
     loadChildren: () => import('../app/pages/place/place.module').then(
       m => m.PlaceModule
     )
