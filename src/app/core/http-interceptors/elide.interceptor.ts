d640c47ff7ca214ff09c2a4876766428118ac55d
couettou <delignyb.pro@gmail.com>
2020-07-06 18:48:04 +0200

    feat: fill interceptor

--- src/app/core/http-interceptors/elide.interceptor.ts
+++ src/app/core/http-interceptors/elide.interceptor.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ElideInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const modified = req.clone({
-      headers: new HttpHeaders({
-        'Content-Type': 'application/vnd.api+json',
-        // 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NDczMDkwMn0.le_DqzsoK1QgmRTSPEls9cyRpg7XNSbTURHbdNtazDnIich6G-zwq1darmF8va38mlNvQAPuDZUmdW9dXrmSUQ'
-      })
-    });
-    return next.handle(modified);
+    let headers;
+    const token = localStorage.getItem('token');
 
+    if (token) {
+      headers = new HttpHeaders({
+        'Content-Type': 'application/vnd.api+json',
+        'Authorization': localStorage.getItem('token')
+      })
+
+    } else {
+      headers = new HttpHeaders({
+        'Content-Type': 'application/vnd.api+json'
+      })
+    }
+    const modified = req.clone({
+      headers
+    });
+
+    return next.handle(modified);
   }
 
 }
