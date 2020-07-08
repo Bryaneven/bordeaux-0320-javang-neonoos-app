cab2de34c6ad5e03b4a50e09d1b3a1a264f36c43
couettou <delignyb.pro@gmail.com>
2020-07-08 11:22:40 +0200

    feat: fill Trip Service

--- src/app/pages/guide/services/trip/trip.service.ts
+++ src/app/pages/guide/services/trip/trip.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RootObjectList } from 'src/app/shared/models/root-object-list.model';
+import { Trip } from 'src/app/shared/models/trip';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getTrips(): Observable<RootObjectList<Trip>> {
+    return this.httpClient.get<RootObjectList<Trip>>(`${environment.APIURI}trips`);
+  }
 }
