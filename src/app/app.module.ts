a51f62f80265067dff1b86125eb8a0bec815bc03
Benoit Deligny <delignyb.pro@gmail.com>
2020-07-13 11:02:10 +0200

    Feat/progbar (#58)
    * feat: import in app module
    
    * feat: create loader service
    
    * feat: add loader interceptor
    
    * feat: create progression Bar component and fill it
    
    * feat: add progBar in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,29 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 // Material
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
 import { ElideInterceptor } from './core/http-interceptors/elide.interceptor';
+import { ProgbarComponent } from './shared/components/progbar/progbar.component';
+import { LoaderService } from './shared/services/loader.service';
+import { LoaderInterceptor } from './core/http-interceptors/loader-interceptor.interceptor';
 import { DialogSaveComponent } from './shared/components/dialog-save/dialog-save.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
+    ProgbarComponent,
     DialogSaveComponent,
+
   ],
 
   imports: [
@@ -36,11 +43,18 @@ import { MatButtonModule } from '@angular/material/button';
     MatIconModule,
     MatInputModule,
     FormsModule,
+    QuillModule.forRoot(),
+    MatProgressBarModule
     MatButtonModule,
     MatDialogModule,
-    QuillModule.forRoot()
   ],
   providers: [
+    LoaderService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ElideInterceptor,
