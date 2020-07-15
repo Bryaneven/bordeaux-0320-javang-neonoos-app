36db08b38c22d127b16da394a086e7acc9851901
Gabriel Touron <gtcode@pm.me>
2020-07-15 10:00:30 +0200

    feat: add place getById in place parent component

--- src/app/pages/place/place-edit-view/place-edit-view.component.ts
+++ src/app/pages/place/place-edit-view/place-edit-view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PlaceService } from '../services/place/place.service';
+import { RootObject } from 'src/app/shared/models/root-object.model';
+import { Place } from 'src/app/shared/models/place.model';
+import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'neo-place-edit-view',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceEditViewComponent implements OnInit {
 
-  constructor() { }
+  subscription = new Subscription();
+  place?: RootObject<Place>;
+  placeId: number;
+
+  constructor(
+    private placeService: PlaceService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getRouteParam();
+  }
+
+  getRouteParam() {
+    const routerSubsription = this.route.paramMap.subscribe(params => {
+      if (params && params.get('id')) {
+        const routePlaceId = Number(params.get('id'));
+        this.placeId = routePlaceId;
+        this.getOnePlace(this.placeId);
+      } else {
+        this.place = new RootObject<Place>(Place, 'places');
+      }
+    });
+    this.subscription.add(routerSubsription);
+  }
+
+
+  getOnePlace(id: number) {
+    const getOnePlaceSubscription = this.placeService.getById(id).subscribe((place: RootObject<Place>) => {
+      if (place) {
+        this.place = place;
+      } else {
+        this.place = new RootObject<Place>(Place, 'places');
+      }
+    });
+    this.subscription.add(getOnePlaceSubscription);
   }
 
 }
