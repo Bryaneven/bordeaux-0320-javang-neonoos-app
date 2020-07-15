f2d17ef6d0f72c1434dc7fd53fdc10545bbf88ef
Gabriel Touron <gtcode@pm.me>
2020-07-15 15:46:01 +0200

    feat: add select for countries

--- src/app/pages/place/place-edit-view/components/place-edit/place-edit.component.ts
+++ src/app/pages/place/place-edit-view/components/place-edit/place-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RootObject } from 'src/app/shared/models/root-object.model';
 import { Place } from 'src/app/shared/models/place.model';
+import { RootObjectList } from 'src/app/shared/models/root-object-list.model';
+import { Country } from 'src/app/shared/models/country';
 
 @Component({
   selector: 'neo-place-edit',
@@ -11,11 +13,22 @@ export class PlaceEditComponent implements OnInit {
 
   @Input() place?: RootObject<Place>;
   @Input() placeId: number;
+  @Input() countries: RootObjectList<Country>;
+  @Input() country: RootObjectList<Country>;
 
   show = true;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  test() {
+    console.log(this.country.data);
+
+  }
+
+  compareObjects(country1: any, country2: any) {
+    return country1.id === country2.id;
+  }
 }
