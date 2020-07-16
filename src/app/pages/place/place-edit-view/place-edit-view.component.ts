65bd7c94a5364ca266775183320eb209343d4741
couettou <delignyb.pro@gmail.com>
2020-07-16 14:37:29 +0200

    feat: get picture by place

--- src/app/pages/place/place-edit-view/place-edit-view.component.ts
+++ src/app/pages/place/place-edit-view/place-edit-view.component.ts
@@ -4,6 +4,9 @@ import { RootObject } from 'src/app/shared/models/root-object.model';
 import { Place } from 'src/app/shared/models/place.model';
 import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
+import { RootObjectList } from 'src/app/shared/models/root-object-list.model';
+import { Picture } from 'src/app/shared/models/picture.model';
+import { ActivityService } from 'src/app/shared/services/activity.service';
 
 @Component({
   selector: 'neo-place-edit-view',
@@ -16,9 +19,12 @@ export class PlaceEditViewComponent implements OnInit {
   place?: RootObject<Place>;
   placeId: number;
 
+  picturesByPlace: RootObjectList<Picture> = new RootObjectList<Picture>(Picture, 'pictures');
+
   constructor(
     private placeService: PlaceService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private activityService: ActivityService,
   ) { }
 
   ngOnInit(): void {
@@ -31,6 +37,7 @@ export class PlaceEditViewComponent implements OnInit {
         const routePlaceId = Number(params.get('id'));
         this.placeId = routePlaceId;
         this.getOnePlace(this.placeId);
+        this.getPicturesByPlace(this.placeId);
       } else {
         this.place = new RootObject<Place>(Place, 'places');
       }
@@ -48,6 +55,12 @@ export class PlaceEditViewComponent implements OnInit {
       }
     });
     this.subscription.add(getOnePlaceSubscription);
+  }
+
+  getPicturesByPlace(id: number) {
+    this.activityService.getPicturesByPlace(id).subscribe(
+      pictures => this.picturesByPlace = pictures
+    );
   }
 
 }
